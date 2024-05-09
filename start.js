0b0b78ea039fb7e52984b0c5d231fda1f56a1af7
cocktailpeanut <cocktailpeanuts@proton.me>
2024-05-09 02:52:20 -0400

    add proxy + set GRADIO_SERVER_NAME as 127.0.0.1

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         venv: "env",                // Edit this to customize the venv folder path
-        env: { },                   // Edit this to customize environment variables (see documentation)
+        env: { "GRADIO_SERVER_NAME": "127.0.0.1" },                   // Edit this to customize environment variables (see documentation)
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "llamafactory-cli webui"
@@ -31,13 +31,12 @@ module.exports = {
         url: "{{input.event[0]}}"
       }
     },
-//    Uncomment this step to enable local wifi sharing (access the app from devices on the same network)
-//    {
-//      method: "proxy.start",
-//      params: {
-//        uri: "{{local.url}}",
-//        name: "Local Sharing"
-//      }
-//    }
+    {
+      method: "proxy.start",
+      params: {
+        uri: "{{local.url}}",
+        name: "Local Sharing"
+      }
+    }
   ]
 }
